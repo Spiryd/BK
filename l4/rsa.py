bdbf10688b7f7c1b7e37151338fbe12ab7a742d7
Spiryd <neumann.maks@gmail.com>
2024-05-21 14:32:01 +0200

    Update rsa.py

--- l4/rsa.py
+++ l4/rsa.py
@@ -32,6 +32,22 @@ def multiplicative_inverse(e, phi):
     
     if temp_phi == 1:
         return d + phi
+    
+def factorize(n, e, d):
+    t = e*d - 1
+    kphi = t
+    while t % 2 == 0:
+        t = t // 2
+    a = 2
+    while a < 1_000_000:
+        k = t 
+        while k < kphi:
+            x = pow(a, k, n)
+            if x != 1 and x != n-1 and pow(x, 2, n) == 1:
+                r = gcd(x-1, n)
+                return r, n//r
+            k = k * 2
+        a = a + 2
 
 def generate_keypair(p, q):
     if not (is_prime(p) and is_prime(q)):
@@ -50,6 +66,8 @@ def generate_keypair(p, q):
 
 p = 4858654271
 q = 7678045183
+print("p: ", p)
+print("q: ", q)
 
 publicA, privateA = generate_keypair(p, q)
 publicB, privateB = generate_keypair(p, q)
@@ -58,6 +76,11 @@ print("PublicA: ", publicA)
 print("PrivateA: ", privateA)
 print("PublicB: ", publicB)
 
-d_B = multiplicative_inverse(publicB[1], (p-1)*(q-1))
-print("PrivateB: ", (publicB[0], d_B))
+factored_p, factored_q = factorize(publicA[0], publicA[1], privateA[1])
+
+print("Factored p: ", factored_p)
+print("Factored q: ", factored_q)
+
+d_B = multiplicative_inverse(publicB[1], (factored_p-1)*(factored_q-1))
+print("Cracked PrivateB: ", (publicB[0], d_B))
 print("PrivateB: ", privateB)
